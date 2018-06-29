8a3f71a1c20b2f930a0dc9d80301a18d0afb0053
Javier Martín <javier@elretirao.net>
2018-06-29 03:30:35 +0200

    Remove all tests except one.
    If I'm correct and travis only fails with the first test, this test
    will always fail.

--- spec/features/debates_spec.rb
+++ spec/features/debates_spec.rb
@@ -2,30 +2,6 @@
 require 'rails_helper'
 
 feature 'Debates' do
-  scenario 'Shows up to 5 suggestions', :js do
-    author = create(:user)
-    login_as(author)
-
-    create(:debate, title: "First debate has 1 vote", cached_votes_up: 1)
-    create(:debate, title: "Second debate has 2 votes", cached_votes_up: 2)
-    create(:debate, title: "Third debate has 3 votes", cached_votes_up: 3)
-    create(:debate, title: "This one has 4 votes", description: "This is the fourth debate", cached_votes_up: 4)
-    create(:debate, title: "Fifth debate has 5 votes", cached_votes_up: 5)
-    create(:debate, title: "Sixth debate has 6 votes", description: 'This is the sixth debate',  cached_votes_up: 6)
-    create(:debate, title: "This has seven votes, and is not suggest", description: 'This is the seven', cached_votes_up: 7)
-
-    puts "#{Time.now - $INITIAL_TIME} starting with debate"
-    visit new_debate_path
-    puts "#{Time.now - $INITIAL_TIME} before filling in with debate"
-    fill_in 'debate_title', with: 'debate'
-    check "debate_terms_of_service"
-    puts "#{Time.now - $INITIAL_TIME} after checking terms with debate"
-
-    within('div#js-suggest') do
-      expect(page).to have_content "You are seeing 5 of 6 debates containing the term 'debate'"
-    end
-  end
-
   scenario 'No found suggestions', :js do
     author = create(:user)
     login_as(author)
