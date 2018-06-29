996f3f985d64ce5a7d505c6880a2882e2725ce36
Javier Martín <javier@elretirao.net>
2018-06-29 02:47:22 +0200

    Remove coveralls reports in travis.
    Let's keep trying to debug why travis builds fail.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,4 @@
 ENV['RAILS_ENV'] ||= 'test'
-if ENV['TRAVIS']
-  require 'coveralls'
-  Coveralls.wear!('rails')
-end
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
